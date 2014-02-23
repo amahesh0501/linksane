cbd724861c8d4ceea40547b14bc810ef0fd0c6f6
Apprentice <apprentice@dbc20.local>
2014-02-23 02:02:39 -0800

    Cleaned up sidebar views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
+
+  before_filter :get_signed_in_walls
+
   protect_from_forgery
+
+  def get_signed_in_walls
+    user = current_user
+    memberships = Membership.where(user_id: current_user.id)
+    @user_walls_accessible = []
+    memberships.each {|membership| @user_walls_accessible << Wall.find(membership.wall_id) if membership.revoked == false}
+  end
 end
